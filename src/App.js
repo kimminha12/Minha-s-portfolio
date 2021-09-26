7e34db2222ecbb72256068d2283684f91cbc1358
kimminha12 <alsgk9298@gmail.com>
2021-09-26 14:47:23 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Loading from "./Loading";
+import React, {Component} from 'react';
+import styled from "styled-components";
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+      <Navbar />
+      <Switch>
+        <Route path=""/>
+        <Route path="" />
+        <Route path="" />
+        <Route path="" />
+      </Switch>
+    </Router>
+    )
+  }
 }
 
 export default App;
